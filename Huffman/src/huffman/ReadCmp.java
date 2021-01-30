de14aea5e4a31f6584e566c14fc1b8fb296d507d
KG <ghazanfarik79@gmail.com>
2021-01-30 21:18:09 +0330

    ReadCmp Class created

---
+++ Huffman/src/huffman/ReadCmp.java
@@ -0,0 +1,35 @@
+package huffman;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class ReadCmp {
+
+    String test() {
+        String test = "";
+        try {
+            File check = new File("Tree.cmp");
+
+            FileReader filereader = new FileReader(check);
+
+            BufferedReader reader = new BufferedReader(filereader);
+            ArrayList<String> lines = new ArrayList<>();
+            String st;
+            while ((st = reader.readLine()) != null) {
+                lines.add(st);
+            }
+            for (int i = 0; i < lines.size(); i++) {
+                test = test + lines.get(i) + "\n";
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return test;
+
+    }
+
+}
